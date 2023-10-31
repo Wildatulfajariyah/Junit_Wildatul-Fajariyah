0cc8dfb8209c14563cc3fe66f69771759f1c78ab
Wildatulfajariyah <wildarehan65@gmail.com>
2023-10-31 11:56:26 +0700

    second commit

--- app/src/main/java/com/wilda/wkplminggu11/Calculator.java
+++ app/src/main/java/com/wilda/wkplminggu11/Calculator.java
@@ -1,8 +1,10 @@
 package com.wilda.wkplminggu11;
 
+// Membuat kelas calculator untuk menghitung
 public class Calculator {
 
+    // Menambahkan dua elemen dengan variabel integer
     public Integer add(Integer first, Integer second) {
-        return  first + second;
+        return  first + second; // berfungsi untuk mengembalikan nilai
     }
 }
